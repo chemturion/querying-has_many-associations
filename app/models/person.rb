7976e184b9ccaf5347c150aaa91ba41457213ea3
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-11-21 18:24:45 -0600

    passing specs for querying has_many self-join

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,14 +5,14 @@ class Person < ActiveRecord::Base
   has_many :employees, class_name: "Person", foreign_key: :manager_id
 
   def self.order_by_location_name
-    all
+    order(:name)
   end
 
   def self.with_employees
-    all
+    where(manager_id: nil)
   end
 
   def self.with_employees_order_by_location_name
-    all
+    with_employees.order_by_location_name
   end
 end
